c0d713727ef803687becf6edf739b483755a5006
kartikeysinghhere <kartikey97999@gmail.com>
2026-05-20 17:06:58 +0530

    fix: switch brevo smtp to port 465

--- config/mailer.js
+++ config/mailer.js
@@ -3,8 +3,8 @@ const env = require('./env');
 
 const transporter = nodemailer.createTransport({
   host: env.BREVO_SMTP_HOST,
-  port: parseInt(env.BREVO_SMTP_PORT) || 587,
-  secure: false,
+  port: parseInt(env.BREVO_SMTP_PORT) || 565,
+  secure: true,
   auth: {
     user: env.BREVO_SMTP_USER,
     pass: env.BREVO_SMTP_PASS
